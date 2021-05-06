903c237dd0347e6dc0c34a2d46a09513323db09c
Saravana Raja <saravanaraja25@gmail.com>
2021-05-06 10:52:17 +0530

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -18,7 +18,7 @@ function Work() {
                                 <h4>Kongu Engineering College - Dept of CT-PG</h4>
                                 <p>Tech Stack : ReactJS , Firebase</p>
                                 <a target="_blank" rel="noreferrer" href="https://github.com/saravanaraja25/kec-msc-react-v2"><i className="fab fa-github"></i></a>
-                                <a target="_blank" rel="noreferrer" href="https://kongumscsoftwaresystems.in"><i className="fas fa-link"></i></a>
+//                                 <a target="_blank" rel="noreferrer" href="https://kongumscsoftwaresystems.in"><i className="fas fa-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -40,7 +40,7 @@ function Work() {
                                 <h4>E-Commerce Application</h4>
                                 <p>Tech Stack : Laravel</p>
                                 <a target="_blank" rel="noreferrer" href="https://github.com/saravanaraja25/b2becom-laravel7"><i className="fab fa-github"></i></a>
-                                <a target="_blank" rel="noreferrer" href="https://ecom.saravanaraja.xyz"><i className="fas fa-link"></i></a>
+//                                 <a target="_blank" rel="noreferrer" href="https://ecom.saravanaraja.xyz"><i className="fas fa-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -49,9 +49,9 @@ function Work() {
                             <img src={git} alt="git"/>
                             <div class="c-content">
                                 <h4>Github User Finder</h4>
-                                <p>Tech Stack : React JS, AWS, Jenkins for CI/CD</p>
+                                <p>Tech Stack : React JS</p>
                                 <a target="_blank" rel="noreferrer" href="https://github.com/saravanaraja25/github-finder"><i className="fab fa-github"></i></a>
-                                <a target="_blank" rel="noreferrer" href="http://3.236.147.106/"><i className="fas fa-link"></i></a>
+//                                 <a target="_blank" rel="noreferrer" href="http://3.236.147.106/"><i className="fas fa-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -62,7 +62,7 @@ function Work() {
                                 <h4>Weather Finder</h4>
                                 <p>Tech Stack : VueJS</p>
                                 <a target="_blank" rel="noreferrer" href="https://github.com/saravanaraja25/weather-app-vue"><i className="fab fa-github"></i></a>
-                                <a target="_blank" rel="noreferrer" href="https://weather.saravanaraja.xyz"><i className="fas fa-link"></i></a>
+//                                 <a target="_blank" rel="noreferrer" href="https://weather.saravanaraja.xyz"><i className="fas fa-link"></i></a>
                             </div>
                         </div>
                     </div>
